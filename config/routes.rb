470d5ae7a81c1f2beca9d85c64b18f45b29c6052
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-09-25 11:44:28 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,31 @@
 Rails.application.routes.draw do
-
-
+  
+# 顧客用
+scope module: :public do
+  root 'homes#top'
+  get 'souvenirs/search_area' => 'souvenir#search_area'
+  get 'souvenirs/search_genre' => 'souvenir#search_genre'
+  get 'souvenirs' => 'souvenirs#index'
+  
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
 }
 
+end
 
+
+
+# 管理者用
+namespace :admin do
+  
+  
+  
+  
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
 }
+
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
