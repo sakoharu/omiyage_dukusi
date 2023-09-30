0af3d163e9da8d13fa21ee7fb011413ce4917aed
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-09-30 06:39:05 +0000

    topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  
+
 # 顧客用
 scope module: :public do
  #root 'homes#top'
   get '/' => 'homes#top'
-  get 'souvenirs/search_area' => 'souvenir#search_area'
-  get 'souvenirs/search_genre' => 'souvenir#search_genre'
-  get 'souvenirs' => 'souvenirs#index'
-  get 'souvenirs/new' => 'souvenirs#new'
-
-
+  resources :souvenirs, only: %i(index new create show) do
+    collection do
+      get :search_area
+      get :search_genre
+    end
+  end
+  
+  resources :souvenirs, only: %i(show edit update destroy) 
 end
 
  devise_for :customers, skip: [:passwords], controllers: {
@@ -21,9 +23,9 @@ end
 
 # 管理者用
 namespace :admin do
-  
- 
-end 
+
+
+end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
