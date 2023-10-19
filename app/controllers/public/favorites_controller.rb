4374d801bfb43958af4da7e90e0479d7ccafa8a6
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-10-19 11:39:00 +0000

    いいね機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,9 +4,17 @@ class Public::FavoritesController < ApplicationController
   end
 
   def create
+    souvenir = Souvenir.find(params[:souvenir_id])
+    favorite = current_customer.favorites.new(souvenir_id: souvenir_id)
+    favorite.save
+    redirect_to souvenirs_path(souvenir)
   end
 
   def destroy
+    souvenir = Souvenir.find(params[:souvenir_id])
+    favorite = current_customer.favorites.find_by(souvenir_id: souvenir_id)
+    favorite.destroy
+    redirect_to souvenirs_path(souvenir)
   end
 
 
