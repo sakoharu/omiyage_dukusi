b394cca1a90a08ccccb9479d23c7b7d298143db2
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-10-26 12:02:57 +0000

    検索ぺーじ

--- app/javascript/packs/japan_map.js
+++ app/javascript/packs/japan_map.js
@@ -1,8 +1,20 @@
 $(function(){
+
+
   $("#map-container").japanMap({
     width:600,
+        backgroundColor : "#f2fcff",
+        borderLineColor: "#f2fcff",
+        borderLineWidth : 0.25,
+        lineColor : "#a0a0a0",
+        lineWidth: 1,
+        drawsBoxLine: true,
+        showsPrefectureName: true,
+        prefectureNameType: "short",
+        movesIslands : true,
+        fontSize : 11,
     onSelect : function(data){
-     alert(data.name);
+     location.href = location.pathname + "?prefecture_id=" + data.code;
     }
   });
 });
